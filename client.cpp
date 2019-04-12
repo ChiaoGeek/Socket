ed2e4637cf1bbcd0b910ac1eab4c5c77c563ac69
zhaochang <zhaochan@usc.edu>
2019-04-12 10:04:01 -0700

    add message to client

--- client.cpp
+++ client.cpp
@@ -87,7 +87,7 @@ void startClient(string arg)
         if(fW.compare("res") == 0) {
             cout << "The delay for link "+v[1]+" is "+v_2[3]+"ms" << endl;
         }else {
-            if(fW.compare("Link")) {
+            if(fW.compare("Link") == 0) {
                 cout << "Link ID not found" << endl;
             }else {
                 cout << "The write operation has been completed successfully" << endl;
